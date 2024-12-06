2d0a0ae77f712bff303bed8933b34ee64b277030
pooya.oa <pooya.oladazimi@tib.eu>
2024-12-06 12:18:09 +0100

    implement tree pprint

--- tree/btree.go
+++ tree/btree.go
@@ -3,6 +3,9 @@ package tree
 import (
 	"Pooya-Oladazimi/goDataStructAndAlg/queue"
 	"errors"
+	"fmt"
+	"math"
+	"strings"
 )
 
 type BtreeNode[T any] struct {
@@ -11,16 +14,83 @@ type BtreeNode[T any] struct {
 	Right *BtreeNode[T]
 }
 
-func BtreeToList[T any](root *BtreeNode[T]) ([]T, error) {
+func (bt *BtreeNode[T]) Height() (int, error) {
+	if bt == nil {
+		return -1, errors.New("node is null.")
+	}
+	treeList, err := bt.ToList()
+	treeNodeCount := 0
+	if err == nil {
+		treeNodeCount = len(treeList) - 1
+	}
+	height := 0
+	for treeNodeCount >= 2 {
+		treeNodeCount /= 2
+		height += 1
+	}
+	return height, nil
+}
+
+func (bt *BtreeNode[T]) pprint() {
+	if bt == nil {
+		fmt.Println("either the tree is empty or the node is a leaf.")
+		return
+	}
+	treeHeight, _ := bt.Height()
+	level := 0
+	q := queue.Newqueue[*BtreeNode[T]](-1)
+	q.Enqueue(bt)
+	indent := treeHeight * 5
+	for level <= treeHeight {
+		levelNodeCap := int64(math.Pow(2.0, float64(level)))
+		line1 := strings.Repeat(" ", indent-(level*2))
+		line2 := ""
+		if level != treeHeight {
+			line2 = strings.Repeat(" ", indent-(level*2)-4)
+		}
+		for levelNodeCap > 0 {
+			cNode, err := q.Dequeue()
+			checkError(err)
+			if cNode == nil {
+				levelNodeCap -= 1
+				q.Enqueue(nil)
+				q.Enqueue(nil)
+				line1 += "  "
+				continue
+			}
+			line1 += fmt.Sprintf("%v", cNode.Node) + " "
+			if cNode.Left != nil {
+				line2 += "  / "
+				q.Enqueue(cNode.Left)
+			} else {
+				line2 += "  "
+				q.Enqueue(nil)
+			}
+			if cNode.Right != nil {
+				line2 += "\\"
+				q.Enqueue(cNode.Right)
+			} else {
+				line2 += " "
+				q.Enqueue(nil)
+			}
+			levelNodeCap -= 1
+		}
+		fmt.Println(line1)
+		fmt.Println(line2)
+		level += 1
+	}
+}
+
+func (bt *BtreeNode[T]) ToList() ([]T, error) {
 	res := make([]T, 0)
-	if root == nil {
-		return res, errors.New("tree is empty")
+	if bt == nil {
+		return res, errors.New("tree is empty or node is leaf.")
 	}
 
 	var firstEmptyElement T
 	res = append(res, firstEmptyElement)
 	q := queue.Newqueue[*BtreeNode[T]](-1)
-	q.Enqueue(root)
+	q.Enqueue(bt)
 	for !q.IsEmpty() {
 		cnode, err := q.Dequeue()
 		checkError(err)
--- tree/tree_test.go
+++ tree/tree_test.go
@@ -1,26 +1,46 @@
 package tree
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestTreeToList(t *testing.T) {
-	root := &BtreeNode[string]{Node: "root"}
-	l1 := &BtreeNode[string]{Node: "l1"}
-	r1 := &BtreeNode[string]{Node: "r1"}
+func getBtreeExample() *BtreeNode[int] {
+	/**
+	    *           1
+	    *          /\
+	    *         2  3
+	  *          /    \
+	  *         4      5
+	  *        /
+	  *       6
+	*/
+	root := &BtreeNode[int]{Node: 1}
+	l1 := &BtreeNode[int]{Node: 2}
+	r1 := &BtreeNode[int]{Node: 3}
 	root.Left = l1
 	root.Right = r1
-	n1 := &BtreeNode[string]{Node: "n1"}
-	n2 := &BtreeNode[string]{Node: "n2"}
-	n3 := &BtreeNode[string]{Node: "n3"}
+	n1 := &BtreeNode[int]{Node: 4}
+	n2 := &BtreeNode[int]{Node: 5}
+	nX := &BtreeNode[int]{Node: 10}
+	n3 := &BtreeNode[int]{Node: 6}
 	l1.Left = n1
 	r1.Right = n2
+	r1.Left = nX
 	n1.Left = n3
-	treeList, err := BtreeToList(root)
-	if err != nil {
-		t.Fatalf("Error: %v", err)
-	}
-	fmt.Println(treeList)
-	fmt.Println(len(treeList))
+	return root
+}
+
+// func TestTreeToList(t *testing.T) {
+// 	root := getBtreeExample()
+// 	treeList, err := root.ToList()
+// 	if err != nil {
+// 		t.Fatalf("Error: %v", err)
+// 	}
+// 	fmt.Println(treeList)
+// 	fmt.Println(len(treeList))
+// }
+
+func TestPprint(t *testing.T) {
+	root := getBtreeExample()
+	root.pprint()
 }
